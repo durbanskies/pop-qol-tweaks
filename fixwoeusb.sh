31f6a10020fe51c43eb1ffe337d00f2670521e8a
durbanskies <75717651+durbanskies@users.noreply.github.com>
2021-06-11 10:18:04 +0530

    Add files via upload

---
+++ fixwoeusb.sh
@@ -0,0 +1,12 @@
+#error looks like this
+#woeusb : Depends: libwxgtk3.0-0v5 (>= 3.0.4+dfsg) but it is not installable
+
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt update
+sudo apt install woeusb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/w/wxwidgets3.0/libwxgtk3.0-0v5_3.0.4+dfsg-3_amd64.deb
+#replace filename with downloaded file
+sudo dpkg -i filename.deb
+sudo apt update
+sudo apt install woeusb
+#select NTFS for flashing windows iso
