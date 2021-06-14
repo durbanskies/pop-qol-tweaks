449a5e21cec995e2fb0b4a1fa080ee67086270d4
durbanskies <75717651+durbanskies@users.noreply.github.com>
2021-06-14 16:14:52 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,6 @@
 #install for ubuntu
 sudo apt update && sudo apt upgrade
-sudo apt install git menulibre papirus-icon-theme gnome-shell-extensions gnome-tweaks flameshot virtualbox wine python3-pip gdebi steam pop-sound-theme totem gparted grub-customizer htop nautilus-admin
+sudo apt install git menulibre papirus-icon-theme gnome-shell-extensions gnome-tweaks flameshot virtualbox wine python3-pip gdebi steam pop-sound-theme transmission totem timeshift gparted grub-customizer htop nautilus-admin
 echo install these
 echo opening in firefox
-firefox https://www.google.com/intl/en_in/chrome/ https://www.sublimetext.com/docs/linux_repositories.html https://code.visualstudio.com/download https://protonvpn.com/support/linux-ubuntu-vpn-setup/ https://zoom.us/download
+firefox https://www.google.com/intl/en_in/chrome/ https://www.sublimetext.com/docs/linux_repositories.html https://code.visualstudio.com/download https://protonvpn.com/support/linux-ubuntu-vpn-setup/ https://snwh.org/paper/download https://zoom.us/download
