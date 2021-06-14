c04aa83ecf9f340a93a766ea40b021980cd131bf
durbanskies <75717651+durbanskies@users.noreply.github.com>
2021-06-14 16:03:09 +0530

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,6 @@
+#install for ubuntu
+sudo apt update && sudo apt upgrade
+sudo apt install git menulibre papirus-icon-theme gnome-shell-extensions gnome-tweaks flameshot virtualbox wine python3-pip gdebi steam pop-sound-theme totem gparted grub-customizer htop nautilus-admin
+echo install these
+echo opening in firefox
+firefox https://www.google.com/intl/en_in/chrome/ https://www.sublimetext.com/docs/linux_repositories.html https://code.visualstudio.com/download https://protonvpn.com/support/linux-ubuntu-vpn-setup/ https://zoom.us/download
