9c37cb2442d1953bac7b8268d5bf6060369b9814
durbanskies <75717651+durbanskies@users.noreply.github.com>
2021-06-27 20:59:56 +0530

    Update fixwoeusb.sh

--- fixwoeusb.sh
+++ fixwoeusb.sh
@@ -1,7 +1,6 @@
 #error looks like this
 #woeusb : Depends: libwxgtk3.0-0v5 (>= 3.0.4+dfsg) but it is not installable
 
-sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt update
 sudo apt install woeusb
 wget http://mirrors.kernel.org/ubuntu/pool/universe/w/wxwidgets3.0/libwxgtk3.0-0v5_3.0.4+dfsg-3_amd64.deb
