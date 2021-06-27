c1b9e5c097dc350a37a9943c224521fad24f3ff6
durbanskies <75717651+durbanskies@users.noreply.github.com>
2021-06-27 15:44:30 +0530

    Delete installstuff.sh

--- installstuff.sh
+++
@@ -1,8 +0,0 @@
-# QUICKLY SETUP
-sudo apt update && sudo apt upgrade
-sudo apt-get remove libreoffice*
-sudo apt install git menulibre papirus-icon-theme gnome-shell-extensions gnome-tweaks flameshot virtualbox wine python3-pip
-echo install these
-echo opening in firefox
-firefox https://www.google.com/intl/en_in/chrome/ https://www.sublimetext.com/docs/linux_repositories.html https://code.visualstudio.com/download https://protonvpn.com/support/linux-ubuntu-vpn-setup/ https://store.steampowered.com/about/ https://zoom.us/download
-echo use eddy !
